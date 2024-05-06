26eaafbeb5cc6280d6ee8de9f067ed28f3a71bbc
MarlonJerold <jeroldmarlon5@gamail.com>
2024-05-05 21:10:04 -0300

    Criação de Dockerfile
    Signed-off-by: MarlonJerold <jeroldmarlon5@gamail.com>

--- src/main/java/com/quackfinances/quackfinances/view/controller/AccountController.java
+++ src/main/java/com/quackfinances/quackfinances/view/controller/AccountController.java
@@ -1,22 +1,14 @@
 package com.quackfinances.quackfinances.view.controller;
 
 import com.quackfinances.quackfinances.model.Account;
-import com.quackfinances.quackfinances.model.AccountType;
-import com.quackfinances.quackfinances.model.UserModel;
-import com.quackfinances.quackfinances.repository.AccountRepository;
-import com.quackfinances.quackfinances.repository.UserRepository;
 import com.quackfinances.quackfinances.services.service.AccountServiceInterface;
 import com.quackfinances.quackfinances.view.controller.dto.AccountCreateDTO;
 import com.quackfinances.quackfinances.view.controller.dto.AccountUserLoginDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/api/account")
 @RestController
--- src/main/java/com/quackfinances/quackfinances/view/controller/CardController.java
+++ src/main/java/com/quackfinances/quackfinances/view/controller/CardController.java
@@ -1,19 +1,12 @@
 package com.quackfinances.quackfinances.view.controller;
 
-import com.quackfinances.quackfinances.model.*;
-import com.quackfinances.quackfinances.repository.CardRepository;
-import com.quackfinances.quackfinances.repository.UserRepository;
 import com.quackfinances.quackfinances.services.service.CardServiceInterface;
-import com.quackfinances.quackfinances.view.controller.dto.AccountUserLoginDTO;
 import com.quackfinances.quackfinances.view.controller.dto.CardRequestDTO;
 import com.quackfinances.quackfinances.view.controller.dto.CardResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
